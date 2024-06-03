1c032a434f45af3f7e5f3e4b2956eaddea75bf21
itwillwork <nurullin_edgar@mail.ru>
2024-06-03 12:32:24 +0300

    collision check for rule change-endpoints-basepath

--- src/rules/change-endpoints-basepath/index.test.ts
+++ src/rules/change-endpoints-basepath/index.test.ts
@@ -1,114 +1,204 @@
 import processor from './index';
 
 describe('change-endpoints-basepath rule', () => {
-  test('regular', () => {
-    const fakeLogger = global.createFakeLogger();
-    const fakeOpenAPIFile = global.createFakeOpenAPIFile({
-      paths: {
-        '/api/v1/pets': {
-          get: {
-            summary: '',
-            tags: [],
-            responses: {},
-          },
-        },
-      },
-    });
-
-    expect(
-      processor.processDocument(
-        fakeOpenAPIFile,
-        {
-          map: { '/api/v1': '' },
-        },
-        fakeLogger
-      )
-    ).toEqual({
-      ...fakeOpenAPIFile,
-      document: {
-        ...fakeOpenAPIFile.document,
-        paths: {
-          '/pets': {
-            get: {
-              summary: '',
-              tags: [],
-              responses: {},
+    test('regular', () => {
+        const fakeLogger = global.createFakeLogger();
+        const fakeOpenAPIFile = global.createFakeOpenAPIFile({
+            paths: {
+                '/api/v1/pets': {
+                    get: {
+                        summary: '',
+                        tags: [],
+                        responses: {},
+                    },
+                },
             },
-          },
-        },
-      },
-    });
+        });
 
-    expect(fakeLogger.warning).toBeCalledTimes(0);
-  });
-
-  test('regular, logger warning', () => {
-    const fakeLogger = global.createFakeLogger();
-    const fakeOpenAPIFile = global.createFakeOpenAPIFile({
-      paths: {
-        '/api/v1/pets': {
-          get: {
-            summary: '',
-            tags: [],
-            responses: {},
-          },
-        },
-      },
-    });
-
-    expect(
-      processor.processDocument(
-        fakeOpenAPIFile,
-        {
-          map: { '/api/v2': '' },
-        },
-        fakeLogger
-      )
-    ).toEqual({
-      ...fakeOpenAPIFile,
-    });
-
-    expect(fakeLogger.warning).toBeCalledLoggerMethod(/Not found endpoints with prefix/, 1);
-  });
-
-  test('usage option: map', () => {
-    const fakeLogger = global.createFakeLogger();
-    const fakeOpenAPIFile = global.createFakeOpenAPIFile({
-      paths: {
-        '/api/v1/pets': {
-          get: {
-            summary: '',
-            tags: [],
-            responses: {},
-          },
-        },
-      },
-    });
-
-    expect(
-      processor.processDocument(
-        fakeOpenAPIFile,
-        {
-          map: { '/api/v1': '/proxy' },
-        },
-        fakeLogger
-      )
-    ).toEqual({
-      ...fakeOpenAPIFile,
-      document: {
-        ...fakeOpenAPIFile.document,
-        paths: {
-          '/proxy/pets': {
-            get: {
-              summary: '',
-              tags: [],
-              responses: {},
+        expect(
+            processor.processDocument(
+                fakeOpenAPIFile,
+                {
+                    map: {'/api/v1': ''},
+                },
+                fakeLogger
+            )
+        ).toEqual({
+            ...fakeOpenAPIFile,
+            document: {
+                ...fakeOpenAPIFile.document,
+                paths: {
+                    '/pets': {
+                        get: {
+                            summary: '',
+                            tags: [],
+                            responses: {},
+                        },
+                    },
+                },
             },
-          },
-        },
-      },
+        });
+
+        expect(fakeLogger.warning).toBeCalledTimes(0);
     });
 
-    expect(fakeLogger.warning).toBeCalledTimes(0);
-  });
+    test('regular, logger warning', () => {
+        const fakeLogger = global.createFakeLogger();
+        const fakeOpenAPIFile = global.createFakeOpenAPIFile({
+            paths: {
+                '/api/v1/pets': {
+                    get: {
+                        summary: '',
+                        tags: [],
+                        responses: {},
+                    },
+                },
+            },
+        });
+
+        expect(
+            processor.processDocument(
+                fakeOpenAPIFile,
+                {
+                    map: {'/api/v2': ''},
+                },
+                fakeLogger
+            )
+        ).toEqual({
+            ...fakeOpenAPIFile,
+        });
+
+        expect(fakeLogger.warning).toBeCalledLoggerMethod(/Not found endpoints with prefix/, 1);
+    });
+
+    test('usage option: map', () => {
+        const fakeLogger = global.createFakeLogger();
+        const fakeOpenAPIFile = global.createFakeOpenAPIFile({
+            paths: {
+                '/api/v1/pets': {
+                    get: {
+                        summary: '',
+                        tags: [],
+                        responses: {},
+                    },
+                },
+            },
+        });
+
+        expect(
+            processor.processDocument(
+                fakeOpenAPIFile,
+                {
+                    map: {'/api/v1': '/proxy'},
+                },
+                fakeLogger
+            )
+        ).toEqual({
+            ...fakeOpenAPIFile,
+            document: {
+                ...fakeOpenAPIFile.document,
+                paths: {
+                    '/proxy/pets': {
+                        get: {
+                            summary: '',
+                            tags: [],
+                            responses: {},
+                        },
+                    },
+                },
+            },
+        });
+
+        expect(fakeLogger.warning).toBeCalledTimes(0);
+    });
+
+    test('operations collision', () => {
+        const fakeLogger = global.createFakeLogger();
+        const fakeOpenAPIFile = global.createFakeOpenAPIFile({
+            paths: {
+                '/proxy/pets': {
+                    get: {
+                        summary: '',
+                        tags: [],
+                        responses: {},
+                    },
+                },
+                '/api/v1/pets': {
+                    get: {
+                        summary: '',
+                        tags: [],
+                        responses: {},
+                    },
+                },
+            },
+        });
+
+        expect(
+            processor.processDocument(
+                fakeOpenAPIFile,
+                {
+                    map: {'/api/v1': '/proxy'},
+                },
+                fakeLogger
+            )
+        ).toEqual({
+            ...fakeOpenAPIFile,
+            document: {
+                ...fakeOpenAPIFile.document,
+            },
+        });
+
+        expect(fakeLogger.warning).toBeCalledTimes(0);
+        expect(fakeLogger.error).toBeCalledLoggerMethod(/operaion conflicts/, 1);
+    });
+
+    test('operations collision, ignoreOperarionCollisions', () => {
+        const fakeLogger = global.createFakeLogger();
+        const fakeOpenAPIFile = global.createFakeOpenAPIFile({
+            paths: {
+                '/proxy/pets': {
+                    get: {
+                        summary: '',
+                        tags: [],
+                        responses: {},
+                    },
+                },
+                '/api/v1/pets': {
+                    get: {
+                        summary: '',
+                        tags: [],
+                        responses: {},
+                    },
+                },
+            },
+        });
+
+        expect(
+            processor.processDocument(
+                fakeOpenAPIFile,
+                {
+                    map: {'/api/v1': '/proxy'},
+                    ignoreOperarionCollisions: true,
+                },
+                fakeLogger
+            )
+        ).toEqual({
+            ...fakeOpenAPIFile,
+            document: {
+                ...fakeOpenAPIFile.document,
+                paths: {
+                    '/proxy/pets': {
+                        get: {
+                            summary: '',
+                            tags: [],
+                            responses: {},
+                        },
+                    },
+                }
+            },
+        });
+
+        expect(fakeLogger.warning).toBeCalledTimes(0);
+        expect(fakeLogger.error).toBeCalledTimes(0);
+    });
 });
--- src/rules/change-endpoints-basepath/index.ts
+++ src/rules/change-endpoints-basepath/index.ts
@@ -1,60 +1,109 @@
-import { RuleProcessorT } from '../../core/rules/processor-models';
-import { z } from 'zod';
-import { forEachSchema } from '../common/utils/iterators/each-schema';
+import {RuleProcessorT} from '../../core/rules/processor-models';
+import {z} from 'zod';
+import {forEachSchema} from '../common/utils/iterators/each-schema';
+import {HttpMethods} from "../common/openapi-models";
+import {forEachOperation} from '../common/utils/iterators/each-operation';
 
 const configSchema = z
-  .object({
-    map: z.record(z.string(), z.string()),
-  })
-  .strict();
+    .object({
+        map: z.record(z.string(), z.string()),
+        ignoreOperarionCollisions: z.boolean().optional(),
+    })
+    .strict();
+
+type OperationHashT = string;
+
+const getOperationHash = ({
+                              method,
+                              path,
+                          }: {
+    method: HttpMethods;
+    path: string;
+}): OperationHashT => `[${method}] - ${path}`;
+
+const changeBasePath = (sourcePath: string, fromPrefix: string, toPrefix: string): string => {
+    return  sourcePath.replace(fromPrefix, toPrefix);
+}
 
 const processor: RuleProcessorT<typeof configSchema> = {
-  configSchema,
-  defaultConfig: {
-    map: {},
-  },
-  processDocument: (openAPIFile, config, logger) => {
-    const { map } = config;
+    configSchema,
+    defaultConfig: {
+        map: {},
+        ignoreOperarionCollisions: false,
+    },
+    processDocument: (openAPIFile, config, logger) => {
+        const {map, ignoreOperarionCollisions} = config;
 
-    let usageCount: Record<string, number> = {};
-    const increaseUsageCount = (fromPrefix: string) => {
-      usageCount[fromPrefix] = (usageCount[fromPrefix] || 0) + 1;
-    };
+        logger.trace(`Check paths ..`);
 
-    logger.trace(`Check paths ..`);
+        if (!ignoreOperarionCollisions) {
+            const sourceOperationHashes: Array<OperationHashT> = [];
+            const collisionOperationHashes: Array<OperationHashT> = [];
 
-    const pathsSchema = openAPIFile.document?.paths;
+            forEachOperation(openAPIFile, ({path, method}) => {
+                const operationHash = getOperationHash({ method, path });
+                sourceOperationHashes.push(operationHash);
+            });
 
-    Object.keys(pathsSchema || {}).forEach((pathKey) => {
-      Object.keys(map).forEach((fromPrefix) => {
-        const toPrefix = map[fromPrefix] || '';
+            forEachOperation(openAPIFile, ({path, method}) => {
+                Object.keys(map).forEach((fromPrefix) => {
+                    const toPrefix = map[fromPrefix] || '';
 
-        if (pathKey.startsWith(fromPrefix)) {
-          logger.trace(`Matching "${pathKey}" with "${fromPrefix}"`);
+                    if (path.startsWith(fromPrefix)) {
+                        const preparedPath = changeBasePath(path, fromPrefix, toPrefix);
 
-          increaseUsageCount(fromPrefix);
+                        const preparedOperationHash = getOperationHash({ method, path: preparedPath });
 
-          const preparedPath = pathKey.replace(fromPrefix, toPrefix);
+                        if (sourceOperationHashes.includes(preparedOperationHash)) {
+                            collisionOperationHashes.push(preparedOperationHash);
+                        }
+                    }
+                });
+            });
 
-          if (pathsSchema) {
-            pathsSchema[preparedPath] = pathsSchema[pathKey];
-            delete pathsSchema[pathKey];
-          }
+            if (collisionOperationHashes?.length) {
+                const errorMessage = `Failed to change endpoint basepath's, operaion conflicts: \n ${collisionOperationHashes.join('\n')}`;
+                logger.error(new Error(errorMessage), errorMessage);
+                return openAPIFile;
+            }
         }
-      });
-    });
 
-    logger.trace(`Usage count: ${JSON.stringify(usageCount)}`);
+        const pathsSchema = openAPIFile.document?.paths;
 
-    Object.keys(map).forEach((fromPrefix) => {
-      if (!usageCount[fromPrefix]) {
-        logger.warning(`Not found endpoints with prefix "${fromPrefix}"`);
-      }
-    });
+        let usageCount: Record<string, number> = {};
+        const increaseUsageCount = (fromPrefix: string) => {
+            usageCount[fromPrefix] = (usageCount[fromPrefix] || 0) + 1;
+        };
 
-    return openAPIFile;
-  },
+        Object.keys(pathsSchema || {}).forEach((pathKey) => {
+            Object.keys(map).forEach((fromPrefix) => {
+                const toPrefix = map[fromPrefix] || '';
+
+                if (pathKey.startsWith(fromPrefix)) {
+                    logger.trace(`Matching "${pathKey}" with "${fromPrefix}"`);
+
+                    increaseUsageCount(fromPrefix);
+
+                    const preparedPath = changeBasePath(pathKey, fromPrefix, toPrefix);
+                    if (pathsSchema) {
+                        pathsSchema[preparedPath] = pathsSchema[pathKey];
+                        delete pathsSchema[pathKey];
+                    }
+                }
+            });
+        });
+
+        logger.trace(`Usage count: ${JSON.stringify(usageCount)}`);
+
+        Object.keys(map).forEach((fromPrefix) => {
+            if (!usageCount[fromPrefix]) {
+                logger.warning(`Not found endpoints with prefix "${fromPrefix}"`);
+            }
+        });
+
+        return openAPIFile;
+    },
 };
 
 export default processor;
-export { configSchema };
+export {configSchema};
