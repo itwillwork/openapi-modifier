1201c8138b83820f4ceb31e5231cee2db0ac0983
itwillwork <nurullin_edgar@mail.ru>
2024-06-03 11:55:26 +0300

    add test collision components, ignoreComponentCollisions

--- src/rules/merge-openapi-spec/index.test.ts
+++ src/rules/merge-openapi-spec/index.test.ts
@@ -238,4 +238,54 @@ describe('merge-openapi-spec rule', () => {
     expect(fakeLogger.warning).toBeCalledTimes(0);
     expect(fakeLogger.error).toBeCalledLoggerMethod(/component conflicts/, 1);
   });
+
+  test('collision components, ignoreComponentCollisions', () => {
+    const fakeLogger = global.createFakeLogger();
+    const fakeOpenAPIFile = global.createFakeOpenAPIFile({
+      components: {
+        schemas: {
+          Pet: {
+            type: "string"
+          }
+        },
+      },
+    });
+
+    expect(
+        processor.processDocument(
+            fakeOpenAPIFile,
+            {
+              path: __dirname + '/__mocks__/collision/components.yaml',
+              ignoreComponentCollisions: true,
+            },
+            fakeLogger
+        )
+    ).toEqual({
+      ...fakeOpenAPIFile,
+        document: {
+          ...fakeOpenAPIFile.document,
+            components: {
+                ...fakeOpenAPIFile.document.components,
+                schemas: {
+                    ...fakeOpenAPIFile.document.components?.schemas,
+                    "Pet": {
+                        "type": "object",
+                        "required": [
+                            "id"
+                        ],
+                        "properties": {
+                            "id": {
+                                "type": "integer",
+                                "format": "int64"
+                            }
+                        }
+                    }
+                }
+            },
+        }
+    });
+
+    expect(fakeLogger.warning).toBeCalledTimes(0);
+    expect(fakeLogger.error).toBeCalledTimes(0);
+  });
 });
