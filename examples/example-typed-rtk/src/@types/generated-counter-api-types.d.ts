cdbce0d5628c751ec38f1207a87c27028b6eb401
itwillwork <edgarnurullin@yandex-team.ru>
2025-05-23 14:23:54 +0300

    generate types

---
+++ examples/example-typed-rtk/src/@types/generated-counter-api-types.d.ts
@@ -0,0 +1,47 @@
+declare namespace Components {
+    namespace Responses {
+        export type ServerError = Schemas.Error;
+    }
+    namespace Schemas {
+        export interface CounterRequest {
+            /**
+             * Counter value to save
+             * example:
+             * 42
+             */
+            value: number; // int32
+        }
+        export interface CounterResponse {
+            /**
+             * Current counter value
+             * example:
+             * 42
+             */
+            value: number; // int32
+        }
+        export interface Error {
+            /**
+             * Error message
+             * example:
+             * Invalid request format
+             */
+            message: string;
+        }
+    }
+}
+declare namespace Paths {
+    namespace GetCounter {
+        namespace Responses {
+            export type $200 = Components.Schemas.CounterResponse;
+            export type $500 = Components.Responses.ServerError;
+        }
+    }
+    namespace SaveCounter {
+        export type RequestBody = Components.Schemas.CounterRequest;
+        namespace Responses {
+            export type $200 = Components.Schemas.CounterResponse;
+            export type $400 = Components.Schemas.Error;
+            export type $500 = Components.Responses.ServerError;
+        }
+    }
+}
